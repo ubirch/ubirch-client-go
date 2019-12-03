a3524261e4c6871b90782f7d3edf80217110d50c
Roxana Meixner <roxana.meixner@gmail.com>
2019-12-03 11:40:49 +0100

    push to docker.io/ubirch

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 make arm
-docker build -t larox/ubirch-go-udp-client:arm64 .
-docker push larox/ubirch-go-udp-client:arm64
+docker build -t ubirch/ubirch-go-udp-client:arm64 .
+docker push ubirch/ubirch-go-udp-client:arm64
 make x86
-docker build -t larox/ubirch-go-udp-client:amd64 .
-docker push larox/ubirch-go-udp-client:amd64
+docker build -t ubirch/ubirch-go-udp-client:amd64 .
+docker push ubirch/ubirch-go-udp-client:amd64
 cat << EOF > template.yaml
-image: larox/ubirch-go-udp-client:latest
+image: ubirch/ubirch-go-udp-client:latest
 manifests:
-  - image: larox/ubirch-go-udp-client:amd64
+  - image: ubirch/ubirch-go-udp-client:amd64
     platform:
       architecture: amd64
       features:
         - sse
       os: linux
-  - image: larox/ubirch-go-udp-client:arm64
+  - image: ubirch/ubirch-go-udp-client:arm64
     platform:
       architecture: arm64
       os: linux
