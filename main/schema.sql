914bc95d1e9d186d25c2b5c6e676097db0b4b02e
Paul <paul@zom.bi>
2020-03-25 20:48:53 +0100

    update schema

--- main/schema.sql
+++ main/schema.sql
@@ -3,10 +3,3 @@ CREATE TABLE IF NOT EXISTS "protocol_context" (
     "json" jsonb  NOT NULL,
     PRIMARY KEY ("id")
 );
-
-CREATE TABLE IF NOT EXISTS "auth" (
-    "uuid"       text NOT NULL,
-    "key"        text NOT NULL,
-    "auth_token" text NOT NULL,
-    PRIMARY KEY ("uuid")
-);
