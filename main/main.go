e04fa3d8dbd7adf98c1e458fb308abcec08c3ccd
Roxana Meixner <roxana.meixner@gmail.com>
2020-04-17 02:02:10 +0200

    updated http server version

--- main/main.go
+++ main/main.go
@@ -113,7 +113,7 @@ func main() {
 	// listen to messages to sign via http
 	httpSrvSign := api.HTTPServer{MessageHandler: msgsToSign, AuthTokens: conf.Devices}
 	wg.Add(1)
-	httpSrvSign.Serve(ctx, &wg)
+	go httpSrvSign.Serve(ctx, &wg)
 
 	// wait forever, exit is handled via shutdown
 	select {}
