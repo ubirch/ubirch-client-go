795147eaf22ee9064387df9323a9875cc5033efe
Roxana Meixner <roxana.meixner@gmail.com>
2020-06-12 16:06:14 +0200

    prefix protocol context file name with stage

--- main/protocol.go
+++ main/protocol.go
@@ -22,7 +22,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/google/uuid"
@@ -40,7 +39,7 @@ type ExtendedProtocol struct {
 }
 
 // INIT sets keys in crypto context and automatically updates keystore in persistent storage
-func (p *ExtendedProtocol) Init(configDir string, filename string, dsn string, keys map[string]string) error {
+func (p *ExtendedProtocol) Init(contextFile string, dsn string, keys map[string]string) error {
 	// check if we want to use a database as persistent storage
 	if dsn != "" {
 		// use the database
@@ -51,7 +50,7 @@ func (p *ExtendedProtocol) Init(configDir string, filename string, dsn string, k
 		p.db = db
 		log.Printf("protocol context will be saved to database")
 	} else {
-		p.contextFile = filepath.Join(configDir, filename)
+		p.contextFile = contextFile
 		log.Printf("protocol context will be saved to file: %s", p.contextFile)
 	}
 
