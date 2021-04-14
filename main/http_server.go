2482452c2121e5387abc9906bc4f865f339cf776
Roxana Meixner <roxana.meixner@ubirch.com>
2021-04-14 13:36:24 +0200

    comment for timeouts

--- main/http_server.go
+++ main/http_server.go
@@ -18,9 +18,9 @@ const (
 	BackendRequestTimeout = 10 * time.Second // time after which requests to the ubirch backend will be canceled
 	GatewayTimeout        = 20 * time.Second // time after which the client sends a 504 response if no timely response could be produced
 	ShutdownTimeout       = 25 * time.Second // time after which the server will be shut down forcefully if graceful shutdown did not happen before
-	ReadTimeout           = 1 * time.Second
-	WriteTimeout          = 30 * time.Second
-	IdleTimeout           = 60 * time.Second
+	ReadTimeout           = 1 * time.Second  // maximum duration for reading the entire request -> low since we only expect requests with small content
+	WriteTimeout          = 30 * time.Second // time after which the connection will be closed if response was not written -> this should never happen
+	IdleTimeout           = 60 * time.Second // time to wait for the next request when keep-alives are enabled
 )
 
 type ServerEndpoint struct {
