1dddf5341dbd38a639d58bb3933ec195e78eb5de
Roxana Meixner <roxana.meixner@ubirch.com>
2021-04-26 11:56:11 +0200

    fixed tests

--- main/config/config_test.go
+++ main/config/config_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-const expectedConfig = `{"devices":null,"secret":"MTIzNDU2Nzg5MDU2Nzg5MA==","env":"","DSN":"","CSR_country":"","CSR_organization":"","TCP_addr":"","TLS":false,"TLSCertFile":"","TLSKeyFile":"","CORS":false,"CORS_origins":null,"debug":false,"logTextFormat":false,"KeyService":"","IdentityService":"","Niomon":"","VerifyService":""}`
+const expectedConfig = `{"devices":null,"secret":"MTIzNDU2Nzg5MDU2Nzg5MA==","env":"","DSN":{"Host":"","User":"","Password":"","database":""},"CSR_country":"","CSR_organization":"","TCP_addr":"","TLS":false,"TLSCertFile":"","TLSKeyFile":"","CORS":false,"CORS_origins":null,"debug":false,"logTextFormat":false,"SecretBytes":null,"KeyService":"","IdentityService":"","Niomon":"","VerifyService":"","ConfigDir":""}`
 
 func TestConfig(t *testing.T) {
 	configBytes := []byte(expectedConfig)
@@ -21,8 +21,6 @@ func TestConfig(t *testing.T) {
 	//if !bytes.Equal(config.SecretBytes, []byte("1234567890567890")) {
 	//	t.Errorf("Failed to load secret from config")
 	//}
-
-
 
 	jsonBytes, err := json.Marshal(config)
 	if err != nil {
