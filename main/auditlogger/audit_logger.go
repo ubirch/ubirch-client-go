02b60ca01c6fc487255d7cd3164fcbc4d9359de7
Roxana Meixner <roxana.meixner@ubirch.com>
2021-05-18 17:31:39 +0200

    removed redundant "message" key from audit log

--- main/auditlogger/audit_logger.go
+++ main/auditlogger/audit_logger.go
@@ -1,7 +1,6 @@
 package auditlogger
 
 import (
-	"fmt"
 	log "github.com/sirupsen/logrus"
 	"time"
 )
@@ -22,16 +21,15 @@ func SetServiceName(serviceName string) {
 	service = serviceName
 }
 
-func getAuditLogFields(action, object, infos string) log.Fields {
+func getAuditLogFields() log.Fields {
 	return log.Fields{
 		"@timestamp":  time.Now().UTC().Format(timeFormat),
 		"@version":    version,
-		"message":     fmt.Sprintf(messageFormat, service, action, object, infos),
 		"logger_name": loggerName,
 		"tags":        []string{auditTag},
 	}
 }
 
 func AuditLog(action, object, infos string) {
-	log.WithFields(getAuditLogFields(action, object, infos)).Infof("")
+	log.WithFields(getAuditLogFields()).Infof(messageFormat, service, action, object, infos)
 }
