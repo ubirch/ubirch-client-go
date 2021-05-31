ee2726f282a6749e7a27cc1ed13d020c5be840d7
Roxana Meixner <roxana.meixner@ubirch.com>
2021-05-31 11:07:35 +0200

    configure postgres DSN string

--- main/config/config.go
+++ main/config/config.go
@@ -51,26 +51,22 @@ var IsDevelopment bool
 
 // configuration of the client
 type Config struct {
-	Devices          map[string]string `json:"devices"`                               // maps UUIDs to backend auth tokens (mandatory)
-	Secret16Base64   string            `json:"secret" envconfig:"secret"`             // 16 bytes secret used to encrypt the key store (mandatory for migration) LEGACY
-	Secret32Base64   string            `json:"secret32" envconfig:"secret32"`         // 32 byte secret used to encrypt the key store (mandatory)
-	RegisterAuth     string            `json:"registerAuth"`                          // auth token needed for new identity registration
-	Env              string            `json:"env"`                                   // the ubirch backend environment [dev, demo, prod], defaults to 'prod'
-	DsnInitContainer bool              `json:"DSN_InitDb" envconfig:"DSN_InitDb"`     // flag to determine if a database should be used for context management
-	DsnHost          string            `json:"DSN_Host" envconfig:"DSN_Host"`         // database host name
-	DsnUser          string            `json:"DSN_User" envconfig:"DSN_User"`         // database user name
-	DsnPassword      string            `json:"DSN_Password" envconfig:"DSN_Password"` // database password
-	DsnDb            string            `json:"DSN_Database" envconfig:"DSN_Database"` // database name
-	CSR_Country      string            `json:"CSR_country"`                           // subject country for public key Certificate Signing Requests
-	CSR_Organization string            `json:"CSR_organization"`                      // subject organization for public key Certificate Signing Requests
-	TCP_addr         string            `json:"TCP_addr"`                              // the TCP address for the server to listen on, in the form "host:port", defaults to ":8080"
-	TLS              bool              `json:"TLS"`                                   // enable serving HTTPS endpoints, defaults to 'false'
-	TLS_CertFile     string            `json:"TLSCertFile"`                           // filename of TLS certificate file name, defaults to "cert.pem"
-	TLS_KeyFile      string            `json:"TLSKeyFile"`                            // filename of TLS key file name, defaults to "key.pem"
-	CORS             bool              `json:"CORS"`                                  // enable CORS, defaults to 'false'
-	CORS_Origins     []string          `json:"CORS_origins"`                          // list of allowed origin hosts, defaults to ["*"]
-	Debug            bool              `json:"debug"`                                 // enable extended debug output, defaults to 'false'
-	LogTextFormat    bool              `json:"logTextFormat"`                         // log in text format for better human readability, default format is JSON
+	Devices          map[string]string `json:"devices"`                              // maps UUIDs to backend auth tokens (mandatory)
+	Secret16Base64   string            `json:"secret" envconfig:"secret"`            // 16 bytes secret used to encrypt the key store (mandatory for migration) LEGACY
+	Secret32Base64   string            `json:"secret32" envconfig:"secret32"`        // 32 byte secret used to encrypt the key store (mandatory)
+	RegisterAuth     string            `json:"registerAuth"`                         // auth token needed for new identity registration
+	Env              string            `json:"env"`                                  // the ubirch backend environment [dev, demo, prod], defaults to 'prod'
+	PostgresDSN      string            `json:"postgresDSN" envconfig:"POSTGRES_DSN"` // data source name for postgres database
+	CSR_Country      string            `json:"CSR_country"`                          // subject country for public key Certificate Signing Requests
+	CSR_Organization string            `json:"CSR_organization"`                     // subject organization for public key Certificate Signing Requests
+	TCP_addr         string            `json:"TCP_addr"`                             // the TCP address for the server to listen on, in the form "host:port", defaults to ":8080"
+	TLS              bool              `json:"TLS"`                                  // enable serving HTTPS endpoints, defaults to 'false'
+	TLS_CertFile     string            `json:"TLSCertFile"`                          // filename of TLS certificate file name, defaults to "cert.pem"
+	TLS_KeyFile      string            `json:"TLSKeyFile"`                           // filename of TLS key file name, defaults to "key.pem"
+	CORS             bool              `json:"CORS"`                                 // enable CORS, defaults to 'false'
+	CORS_Origins     []string          `json:"CORS_origins"`                         // list of allowed origin hosts, defaults to ["*"]
+	Debug            bool              `json:"debug"`                                // enable extended debug output, defaults to 'false'
+	LogTextFormat    bool              `json:"logTextFormat"`                        // log in text format for better human readability, default format is JSON
 	SecretBytes32    []byte            // the decoded 32 byte key store secret for database (set automatically)
 	KeyService       string            // key service URL (set automatically)
 	IdentityService  string            // identity service URL (set automatically)
