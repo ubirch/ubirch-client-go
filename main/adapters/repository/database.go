49bd690cc8f6e70756060bd3f46d118d6239bd99
Roxana Meixner <roxana.meixner@ubirch.com>
2021-06-14 14:36:32 +0200

    rolled back isolation lvl to "LevelReadCommitted", needs to be tested more

--- main/adapters/repository/database.go
+++ main/adapters/repository/database.go
@@ -59,7 +59,7 @@ func NewSqlDatabaseInfo(dataSourceName, tableName string) (*DatabaseManager, err
 
 	dbManager := &DatabaseManager{
 		options: &sql.TxOptions{
-			Isolation: sql.LevelSerializable,
+			Isolation: sql.LevelReadCommitted,
 			ReadOnly:  false,
 		},
 		db:        pg,
