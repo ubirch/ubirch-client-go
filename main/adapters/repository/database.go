2bc621511d33a23fb822c1b418aa9b7425aaec57
Roxana Meixner <roxana.meixner@ubirch.com>
2021-07-05 12:39:09 +0200

    [WIP] merge from main branch

--- main/adapters/repository/database.go
+++ main/adapters/repository/database.go
@@ -18,37 +18,34 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/lib/pq"
-	log "github.com/sirupsen/logrus"
-	"github.com/ubirch/ubirch-client-go/main/config"
 	"github.com/ubirch/ubirch-client-go/main/ent"
-	"github.com/ubirch/ubirch-client-go/main/vars"
-	"time"
-	// postgres driver is imported for side effects
-	// import pq driver this way only if we dont need it here
-	// done for database/sql (pg, err := sql.Open..)
-	//_ "github.com/lib/pq"
 
+	log "github.com/sirupsen/logrus"
 )
 
-// DatabaseManagerPostgres contains the postgres database connection, and offers methods
+const (
+	PostgreSql string = "postgres"
+)
+
+// DatabaseManager contains the postgres database connection, and offers methods
 // for interacting with the database.
-type DatabaseManagerPostgres struct {
-	options *sql.TxOptions
-	db      *sql.DB
+type DatabaseManager struct {
+	options   *sql.TxOptions
+	db        *sql.DB
+	tableName string
 }
 
 // Ensure Database implements the ContextManager interface
-var _ ContextManager = (*DatabaseManagerPostgres)(nil)
+var _ ContextManager = (*DatabaseManager)(nil)
 
 // NewSqlDatabaseInfo takes a database connection string, returns a new initialized
 // database.
-func NewPostgresSqlDatabaseInfo(conf config.Config) (*DatabaseManagerPostgres, error) {
-	dataSourceName := fmt.Sprintf("host=%s user=%s password=%s port=%d dbname=%s sslmode=disable",
-		conf.DsnHost, conf.DsnUser, conf.DsnPassword, vars.PostgreSqlPort, conf.DsnDb)
-
-	pg, err := sql.Open(vars.PostgreSql, dataSourceName)
+func NewSqlDatabaseInfo(dataSourceName, tableName string) (*DatabaseManager, error) {
+	pg, err := sql.Open(PostgreSql, dataSourceName)
 	if err != nil {
 		return nil, err
 	}
@@ -61,28 +58,30 @@ func NewPostgresSqlDatabaseInfo(conf config.Config) (*DatabaseManagerPostgres, e
 
 	log.Print("preparing postgres usage")
 
-	dbManager := &DatabaseManagerPostgres{
+	dbManager := &DatabaseManager{
 		options: &sql.TxOptions{
 			Isolation: sql.LevelReadCommitted,
 			ReadOnly:  false,
 		},
-		db: pg,
+		db:        pg,
+		tableName: tableName,
 	}
 
-	if _, err = dbManager.db.Exec(CREATE[PostgresIdentity]); err != nil {
+	if _, err = dbManager.db.Exec(CreateTable(PostgresIdentity, tableName)); err != nil {
 		return nil, err
 	}
 
 	return dbManager, nil
 }
 
-func (dm *DatabaseManagerPostgres) Exists(uid uuid.UUID) (bool, error) {
+func (dm *DatabaseManager) Exists(uid uuid.UUID) (bool, error) {
 	var id string
 
-	err := dm.db.QueryRow("SELECT uid FROM identity WHERE uid = $1", uid.String()).
-		Scan(&id)
+	query := fmt.Sprintf("SELECT uid FROM %s WHERE uid = $1", dm.tableName)
+
+	err := dm.db.QueryRow(query, uid.String()).Scan(&id)
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.Exists(uid)
 		}
 		if err == sql.ErrNoRows {
@@ -95,13 +94,14 @@ func (dm *DatabaseManagerPostgres) Exists(uid uuid.UUID) (bool, error) {
 	}
 }
 
-func (dm *DatabaseManagerPostgres) GetPrivateKey(uid uuid.UUID) ([]byte, error) {
+func (dm *DatabaseManager) GetPrivateKey(uid uuid.UUID) ([]byte, error) {
 	var privateKey []byte
 
-	err := dm.db.QueryRow("SELECT private_key FROM identity WHERE uid = $1", uid.String()).
-		Scan(&privateKey)
+	query := fmt.Sprintf("SELECT private_key FROM %s WHERE uid = $1", dm.tableName)
+
+	err := dm.db.QueryRow(query, uid.String()).Scan(&privateKey)
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.GetPrivateKey(uid)
 		}
 		return nil, err
@@ -110,13 +110,14 @@ func (dm *DatabaseManagerPostgres) GetPrivateKey(uid uuid.UUID) ([]byte, error) 
 	return privateKey, nil
 }
 
-func (dm *DatabaseManagerPostgres) GetPublicKey(uid uuid.UUID) ([]byte, error) {
+func (dm *DatabaseManager) GetPublicKey(uid uuid.UUID) ([]byte, error) {
 	var publicKey []byte
 
-	err := dm.db.QueryRow("SELECT public_key FROM identity WHERE uid = $1", uid.String()).
-		Scan(&publicKey)
+	query := fmt.Sprintf("SELECT public_key FROM %s WHERE uid = $1", dm.tableName)
+
+	err := dm.db.QueryRow(query, uid.String()).Scan(&publicKey)
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.GetPublicKey(uid)
 		}
 		return nil, err
@@ -125,13 +126,14 @@ func (dm *DatabaseManagerPostgres) GetPublicKey(uid uuid.UUID) ([]byte, error) {
 	return publicKey, nil
 }
 
-func (dm *DatabaseManagerPostgres) GetAuthToken(uid uuid.UUID) (string, error) {
+func (dm *DatabaseManager) GetAuthToken(uid uuid.UUID) (string, error) {
 	var authToken string
 
-	err := dm.db.QueryRow("SELECT auth_token FROM identity WHERE uid = $1", uid.String()).
-		Scan(&authToken)
+	query := fmt.Sprintf("SELECT auth_token FROM %s WHERE uid = $1", dm.tableName)
+
+	err := dm.db.QueryRow(query, uid.String()).Scan(&authToken)
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.GetAuthToken(uid)
 		}
 		return "", err
@@ -140,13 +142,13 @@ func (dm *DatabaseManagerPostgres) GetAuthToken(uid uuid.UUID) (string, error) {
 	return authToken, nil
 }
 
-func (dm *DatabaseManagerPostgres) StartTransaction(ctx context.Context) (transactionCtx interface{}, err error) {
+func (dm *DatabaseManager) StartTransaction(ctx context.Context) (transactionCtx interface{}, err error) {
 	return dm.db.BeginTx(ctx, dm.options)
 }
 
 // StartTransactionWithLock starts a transaction and acquires a lock on the row with the specified uuid as key.
 // Returns error if row does not exist.
-func (dm *DatabaseManagerPostgres) StartTransactionWithLock(ctx context.Context, uid uuid.UUID) (transactionCtx interface{}, err error) {
+func (dm *DatabaseManager) StartTransactionWithLock(ctx context.Context, uid uuid.UUID) (transactionCtx interface{}, err error) {
 	tx, err := dm.db.BeginTx(ctx, dm.options)
 	if err != nil {
 		return nil, err
@@ -154,11 +156,12 @@ func (dm *DatabaseManagerPostgres) StartTransactionWithLock(ctx context.Context,
 
 	var id string
 
+	query := fmt.Sprintf("SELECT uid FROM %s WHERE uid = $1 FOR UPDATE", dm.tableName)
+
 	// lock row FOR UPDATE
-	err = tx.QueryRow("SELECT uid FROM identity WHERE uid = $1 FOR UPDATE", uid).
-		Scan(&id)
+	err = tx.QueryRow(query, uid).Scan(&id)
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.StartTransactionWithLock(ctx, uid)
 		}
 		return nil, err
@@ -167,7 +170,7 @@ func (dm *DatabaseManagerPostgres) StartTransactionWithLock(ctx context.Context,
 	return tx, nil
 }
 
-func (dm *DatabaseManagerPostgres) CloseTransaction(transactionCtx interface{}, commit bool) error {
+func (dm *DatabaseManager) CloseTransaction(transactionCtx interface{}, commit bool) error {
 	tx, ok := transactionCtx.(*sql.Tx)
 	if !ok {
 		return fmt.Errorf("transactionCtx for database manager is not of expected type *sql.Tx")
@@ -180,7 +183,7 @@ func (dm *DatabaseManagerPostgres) CloseTransaction(transactionCtx interface{}, 
 	}
 }
 
-func (dm *DatabaseManagerPostgres) FetchIdentity(transactionCtx interface{}, uid uuid.UUID) (*ent.Identity, error) {
+func (dm *DatabaseManager) FetchIdentity(transactionCtx interface{}, uid uuid.UUID) (*ent.Identity, error) {
 	tx, ok := transactionCtx.(*sql.Tx)
 	if !ok {
 		return nil, fmt.Errorf("transactionCtx for database manager is not of expected type *sql.Tx")
@@ -188,10 +191,11 @@ func (dm *DatabaseManagerPostgres) FetchIdentity(transactionCtx interface{}, uid
 
 	var id ent.Identity
 
-	err := tx.QueryRow("SELECT * FROM identity WHERE uid = $1", uid.String()).
-		Scan(&id.Uid, &id.PrivateKey, &id.PublicKey, &id.Signature, &id.AuthToken)
+	query := fmt.Sprintf("SELECT * FROM %s WHERE uid = $1", dm.tableName)
+
+	err := tx.QueryRow(query, uid.String()).Scan(&id.Uid, &id.PrivateKey, &id.PublicKey, &id.Signature, &id.AuthToken)
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.FetchIdentity(tx, uid)
 		}
 		return nil, err
@@ -200,17 +204,17 @@ func (dm *DatabaseManagerPostgres) FetchIdentity(transactionCtx interface{}, uid
 	return &id, nil
 }
 
-func (dm *DatabaseManagerPostgres) SetSignature(transactionCtx interface{}, uid uuid.UUID, signature []byte) error {
+func (dm *DatabaseManager) SetSignature(transactionCtx interface{}, uid uuid.UUID, signature []byte) error {
 	tx, ok := transactionCtx.(*sql.Tx)
 	if !ok {
 		return fmt.Errorf("transactionCtx for database manager is not of expected type *sql.Tx")
 	}
 
-	_, err := tx.Exec(
-		"UPDATE identity SET signature = $1 WHERE uid = $2;",
-		&signature, uid.String())
+	query := fmt.Sprintf("UPDATE %s SET signature = $1 WHERE uid = $2;", dm.tableName)
+
+	_, err := tx.Exec(query, &signature, uid.String())
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.SetSignature(tx, uid, signature)
 		}
 		return err
@@ -219,7 +223,7 @@ func (dm *DatabaseManagerPostgres) SetSignature(transactionCtx interface{}, uid 
 	return nil
 }
 
-func (dm *DatabaseManagerPostgres) StoreNewIdentity(transactionCtx interface{}, identity *ent.Identity) error {
+func (dm *DatabaseManager) StoreNewIdentity(transactionCtx interface{}, identity *ent.Identity) error {
 	tx, ok := transactionCtx.(*sql.Tx)
 	if !ok {
 		return fmt.Errorf("transactionCtx for database manager is not of expected type *sql.Tx")
@@ -228,30 +232,32 @@ func (dm *DatabaseManagerPostgres) StoreNewIdentity(transactionCtx interface{}, 
 	// make sure identity does not exist yet
 	var id string
 
-	err := tx.QueryRow("SELECT uid FROM identity WHERE uid = $1 FOR UPDATE", identity.Uid).
-		Scan(&id)
+	query := fmt.Sprintf("SELECT uid FROM %s WHERE uid = $1 FOR UPDATE;", dm.tableName)
+
+	err := tx.QueryRow(query, identity.Uid).Scan(&id)
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.StoreNewIdentity(tx, identity)
 		}
 		if err == sql.ErrNoRows {
 			// there were no rows, but otherwise no error occurred
+			return dm.storeIdentity(tx, identity)
 		} else {
 			return err
 		}
 	} else {
 		return ErrExists
 	}
-
-	return dm.storeIdentity(tx, identity)
 }
 
-func (dm *DatabaseManagerPostgres) storeIdentity(tx *sql.Tx, identity *ent.Identity) error {
-	_, err := tx.Exec(
-		"INSERT INTO identity (uid, private_key, public_key, signature, auth_token) VALUES ($1, $2, $3, $4, $5);",
-		&identity.Uid, &identity.PrivateKey, &identity.PublicKey, &identity.Signature, &identity.AuthToken)
+func (dm *DatabaseManager) storeIdentity(tx *sql.Tx, identity *ent.Identity) error {
+	query := fmt.Sprintf(
+		"INSERT INTO %s (uid, private_key, public_key, signature, auth_token) VALUES ($1, $2, $3, $4, $5);",
+		dm.tableName)
+
+	_, err := tx.Exec(query, &identity.Uid, &identity.PrivateKey, &identity.PublicKey, &identity.Signature, &identity.AuthToken)
 	if err != nil {
-		if IsConnectionAvailableErrorPostgresSql(err) {
+		if dm.isConnectionError(err) {
 			return dm.storeIdentity(tx, identity)
 		}
 		return err
@@ -260,7 +266,7 @@ func (dm *DatabaseManagerPostgres) storeIdentity(tx *sql.Tx, identity *ent.Ident
 	return nil
 }
 
-func IsConnectionAvailableErrorPostgresSql(err error) bool {
+func (dm *DatabaseManager) isConnectionError(err error) bool {
 	if err.Error() == pq.ErrorCode("53300").Name() || // "53300": "too_many_connections",
 		err.Error() == pq.ErrorCode("53400").Name() { // "53400": "configuration_limit_exceeded",
 		time.Sleep(100 * time.Millisecond)
