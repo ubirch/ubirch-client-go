eb6f96513006da9ec558f8169f1c79bbb9e40e47
Roxana Meixner <roxana.meixner@ubirch.com>
2021-11-04 11:26:51 +0100

    log CSR creation

--- main/adapters/handlers/identity_handler.go
+++ main/adapters/handlers/identity_handler.go
@@ -153,6 +153,8 @@ func (i *IdentityHandler) submitCSROrLogError(uid uuid.UUID, csr []byte) {
 }
 
 func (i *IdentityHandler) CreateCSR(uid uuid.UUID) (csrPEM []byte, err error) {
+	log.Infof("%s: creating CSR", uid)
+
 	initialized, err := i.Protocol.IsInitialized(uid)
 	if err != nil {
 		return nil, fmt.Errorf("could not check if identity is already initialized: %v", err)
