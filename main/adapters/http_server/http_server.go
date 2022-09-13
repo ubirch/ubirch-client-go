07b0bbb4e9cb480f0c23184f8c69849c81e16186
Roxana Meixner <roxana.meixner@ubirch.com>
2022-09-13 13:06:54 +0200

    refactoring: move initialization of HTTP endpoints from main to func InitHTTPServer in http_server.go for testability

--- main/adapters/http_server/http_server.go
+++ main/adapters/http_server/http_server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/cors"
+	"github.com/ubirch/ubirch-client-go/main/config"
 
 	log "github.com/sirupsen/logrus"
 	prom "github.com/ubirch/ubirch-client-go/main/prometheus"
@@ -30,6 +31,105 @@ func NewRouter() *chi.Mux {
 	router.Use(prom.PromMiddleware)
 	router.Use(middleware.Timeout(GatewayTimeout))
 	return router
+}
+
+func InitHTTPServer(conf *config.Config,
+	initialize InitializeIdentity, getCSR GetCSR,
+	checkAuth CheckAuth, sign Sign,
+	verify Verify, verifyOffline VerifyOffline,
+	deactivate UpdateActivateStatus, reactivate UpdateActivateStatus,
+	serverID string, readinessChecks []func() error) *HTTPServer {
+
+	httpServer := &HTTPServer{
+		Router:   NewRouter(),
+		Addr:     conf.TCP_addr,
+		TLS:      conf.TLS,
+		CertFile: conf.TLS_CertFile,
+		KeyFile:  conf.TLS_KeyFile,
+	}
+
+	if conf.CORS && config.IsDevelopment { // never enable CORS on production stage
+		httpServer.SetUpCORS(conf.CORS_Origins, conf.Debug)
+	}
+
+	// set up endpoints for liveness and readiness checks
+	httpServer.Router.Get(LivenessCheckEndpoint, Health(serverID))
+	httpServer.Router.Get(ReadinessCheckEndpoint, Ready(serverID, readinessChecks))
+
+	// set up metrics
+	httpServer.Router.Method(http.MethodGet, MetricsEndpoint, prom.Handler())
+
+	// set up endpoint for identity registration
+	httpServer.Router.Put(RegisterEndpoint, Register(conf.RegisterAuth, initialize))
+
+	// set up endpoint for CSR creation
+	fetchCSREndpoint := path.Join(UUIDPath, CSREndpoint) // /<uuid>/csr
+	httpServer.Router.Get(fetchCSREndpoint, FetchCSR(conf.RegisterAuth, getCSR))
+
+	// set up endpoint for key status updates (de-/re-activation)
+	httpServer.Router.Put(ActiveUpdateEndpoint, UpdateActive(conf.RegisterAuth, deactivate, reactivate))
+
+	// set up endpoints for signing
+	signingService := &SigningService{
+		CheckAuth: checkAuth,
+		Sign:      sign,
+	}
+
+	// chain:              /<uuid>
+	// chain hash:         /<uuid>/hash
+	// chain offline:      /<uuid>/offline
+	// chain offline hash: /<uuid>/offline/hash
+	httpServer.AddServiceEndpoint(UUIDPath,
+		signingService.HandleRequest(ChainHash),
+		true,
+	)
+
+	// sign:              /<uuid>/anchor
+	// sign hash:         /<uuid>/anchor/hash
+	// sign offline:      /<uuid>/anchor/offline
+	// sign offline hash: /<uuid>/anchor/offline/hash
+	httpServer.AddServiceEndpoint(path.Join(UUIDPath, string(AnchorHash)),
+		signingService.HandleRequest(AnchorHash),
+		true,
+	)
+
+	// disable:      /<uuid>/disable
+	// disable hash: /<uuid>/disable/hash
+	httpServer.AddServiceEndpoint(path.Join(UUIDPath, string(DisableHash)),
+		signingService.HandleRequest(DisableHash),
+		false,
+	)
+
+	// enable:      /<uuid>/enable
+	// enable hash: /<uuid>/enable/hash
+	httpServer.AddServiceEndpoint(path.Join(UUIDPath, string(EnableHash)),
+		signingService.HandleRequest(EnableHash),
+		false,
+	)
+
+	// delete:      /<uuid>/delete
+	// delete hash: /<uuid>/delete/hash
+	httpServer.AddServiceEndpoint(path.Join(UUIDPath, string(DeleteHash)),
+		signingService.HandleRequest(DeleteHash),
+		false,
+	)
+
+	// set up endpoints for verification
+	verificationService := &VerificationService{
+		Verify:        verify,
+		VerifyOffline: verifyOffline,
+	}
+
+	// verify:              /verify
+	// verify hash:         /verify/hash
+	// verify offline:      /verify/offline
+	// verify offline hash: /verify/offline/hash
+	httpServer.AddServiceEndpoint(VerifyPath,
+		verificationService.HandleRequest,
+		true,
+	)
+
+	return httpServer
 }
 
 func (srv *HTTPServer) SetUpCORS(allowedOrigins []string, debug bool) {
