ad13f4e642a85b477127880ac370f6c2ed6151ed
Roxana Meixner <roxana.meixner@ubirch.com>
2022-11-25 15:06:36 +0100

    add integration test cases for offline anchor and anchor/hash endpoints

--- integration-test/test_integration.py
+++ integration-test/test_integration.py
@@ -237,3 +237,44 @@ class TestIntegration:
 
         assert verify_res.status_code == 200
         assert verify_res.json()["upp"] == res.json()["upp"]
+
+    def test_anchor_offline(self):
+        url = self.host + f"/{self.uuid}/anchor/offline"
+        header = {'Content-Type': 'application/json', 'X-Auth-Token': self.pwd}
+        data_json = get_random_json()
+        data_hash_64 = to_base64(hash_bytes(serialize(data_json)))
+
+        res = requests.post(url, json=data_json, headers=header)
+
+        assert res.status_code == 200
+        upp = binascii.a2b_base64(res.json()["upp"])
+        assert upp[0] == 0x95 and upp[1] == 0x22
+        assert res.json()["hash"] == data_hash_64
+        assert res.json()["publicKey"] == requests.get(self.pubkey_url).json()[0]["pubKeyInfo"]["pubKey"]
+        with pytest.raises(KeyError):
+            res.json()["response"]
+
+        # make sure hash is unknown by ubirch verification service
+        verify_res = requests.post(self.verify_url, data=data_hash_64, headers={'Content-Type': 'text/plain'})
+
+        assert verify_res.status_code == 404
+
+    def test_anchor_offline_hash(self):
+        url = self.host + f"/{self.uuid}/anchor/offline/hash"
+        header = {'Content-Type': 'text/plain', 'X-Auth-Token': self.pwd}
+        data_hash_64 = to_base64(random.randbytes(32))
+
+        res = requests.post(url, data=data_hash_64, headers=header)
+
+        assert res.status_code == 200
+        upp = binascii.a2b_base64(res.json()["upp"])
+        assert upp[0] == 0x95 and upp[1] == 0x22
+        assert res.json()["hash"] == data_hash_64
+        assert res.json()["publicKey"] == requests.get(self.pubkey_url).json()[0]["pubKeyInfo"]["pubKey"]
+        with pytest.raises(KeyError):
+            res.json()["response"]
+
+        # make sure hash is unknown by ubirch verification service
+        verify_res = requests.post(self.verify_url, data=data_hash_64, headers={'Content-Type': 'text/plain'})
+
+        assert verify_res.status_code == 404
