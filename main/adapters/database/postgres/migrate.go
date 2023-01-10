00289c63e4dd3405fd1401a44e6464572932d433
Roxana Meixner <roxana.meixner@ubirch.com>
2023-01-10 13:20:33 +0100

    add 0001_initial.down.sql, wrap schema migration file content in BEGIN and COMMIT statements, make upwards migration more robust by executing each migration step within a transaction and rolling changes as well as schema version back to last clean version if step failed

--- main/adapters/database/postgres/migrate.go
+++ main/adapters/database/postgres/migrate.go
@@ -30,7 +30,7 @@ func (l *Log) Verbose() bool {
 	return log.IsLevelEnabled(log.DebugLevel) || l.verbose
 }
 
-func Migrate(pgxConn *pgx.Conn) {
+func getMigrator(pgxConn *pgx.Conn) *migrate.Migrate {
 	sourceInstance, err := httpfs.New(http.FS(migrations), "migrations")
 	if err != nil {
 		log.Fatalf("could not create new migrate source driver: %v", err)
@@ -40,17 +40,60 @@ func Migrate(pgxConn *pgx.Conn) {
 	//  instead of re-using the connection.
 	dsn := pgxConn.Config().ConnString()
 
-	migrator, err := migrate.NewWithSourceInstance(
-		"httpfs", sourceInstance,
-		dsn,
-	)
+	migrator, err := migrate.NewWithSourceInstance("httpfs", sourceInstance, dsn)
 	if err != nil {
 		log.Fatalf("could not create migrator: %v", err)
 	}
 
-	migrator.Log = &Log{verbose: false}
+	migrator.Log = &Log{}
+	return migrator
+}
 
-	err = migrator.Up()
+func MigrateUp(pgxConn *pgx.Conn) {
+	migrator := getMigrator(pgxConn)
+
+	err := migrator.Up()
+	if err == migrate.ErrNoChange {
+		version, dirty, err := migrator.Version()
+		if dirty {
+			log.Fatalf("database schema is dirty, needs to be manually fixed. Schema version: %d", version)
+		}
+		if err != nil {
+			log.Fatalf("database is migrated, but could not fetch schema version information. Error: %s", err)
+		}
+		// this is fine.
+		log.Infof("nothing to migrate. Schema is already at version: %d", version)
+		return
+	} else if err != nil {
+		log.Errorf("could not migrate database. Error: %s", err)
+
+		// since every migration step is executed within a transaction, changes will be rolled back
+		// if something goes wrong. We can then safely set the schema version to the last clean version
+		// and remove the dirty flag, which is set automatically if a migration step failed.
+		version, dirty, errVersion := migrator.Version()
+		if dirty && version > 0 {
+			lastCleanVersion := int(version) - 1
+			log.Infof("forcing from dirty schema version %d down to %d", version, lastCleanVersion)
+
+			err := migrator.Force(lastCleanVersion)
+			if err != nil {
+				log.Fatalf("forcing schema version to %d failed: %v", lastCleanVersion, err)
+			}
+		} else {
+			log.Fatalf("unable to fix schema version after failed migration. Version: %d, Dirty: %v, Error: %v", version, dirty, errVersion)
+		}
+
+		log.Fatalf("database migration failed for version %d", version)
+	}
+	version, _, _ := migrator.Version()
+	log.Infof("database schema was migrated. New schema version: %d", version)
+
+}
+
+func Migrate(pgxConn *pgx.Conn, targetVersion uint) {
+	migrator := getMigrator(pgxConn)
+
+	err := migrator.Migrate(targetVersion)
 	if err == migrate.ErrNoChange {
 		version, dirty, err := migrator.Version()
 		if dirty {
