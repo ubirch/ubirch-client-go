52e7b3752137890d22b970e0cce2763af10c4b01
Roxana Meixner <roxana.meixner@ubirch.com>
2023-01-18 11:29:48 +0100

    add test cases for repeated migration and wrong driver

--- main/adapters/database/migrate_test.go
+++ main/adapters/database/migrate_test.go
@@ -32,6 +32,33 @@ func TestMigrateUp_Postgres(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestMigrateUp_Repeat_Postgres(t *testing.T) {
+	// this test communicates with the actual postgres database
+	if testing.Short() {
+		t.Skipf("skipping integration test %s in short mode", t.Name())
+	}
+
+	c, err := getConfig()
+	require.NoError(t, err)
+
+	db, err := sql.Open(PostgreSQL, c.DbDSN)
+	require.NoError(t, err)
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	// migrate database schema to the latest version
+	err = MigrateUp(db, PostgreSQL)
+	require.NoError(t, err)
+
+	// try to migrate database schema to the latest version again
+	err = MigrateUp(db, PostgreSQL)
+	require.NoError(t, err)
+}
+
 func TestMigrate_Postgres(t *testing.T) {
 	// this test communicates with the actual postgres database
 	if testing.Short() {
@@ -93,6 +120,27 @@ func TestMigrateUp_Sqlite(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestMigrateUp_Repeat_Sqlite(t *testing.T) {
+	var dsn = filepath.Join(t.TempDir(), testSQLiteDSN+sqliteConfig)
+
+	db, err := sql.Open(SQLite, dsn)
+	require.NoError(t, err)
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	// migrate database schema to the latest version
+	err = MigrateUp(db, SQLite)
+	require.NoError(t, err)
+
+	// try to migrate database schema to the latest version again
+	err = MigrateUp(db, SQLite)
+	require.NoError(t, err)
+}
+
 func TestMigrate_Sqlite(t *testing.T) {
 	var dsn = filepath.Join(t.TempDir(), testSQLiteDSN+sqliteConfig)
 
@@ -127,4 +175,43 @@ func TestMigrate_Down_Sqlite(t *testing.T) {
 
 	err = MigrateDown(db, SQLite)
 	require.NoError(t, err)
+}
+
+func TestMigrate_WrongDriver_Postgres(t *testing.T) {
+	// this test communicates with the actual postgres database
+	if testing.Short() {
+		t.Skipf("skipping integration test %s in short mode", t.Name())
+	}
+
+	c, err := getConfig()
+	require.NoError(t, err)
+
+	db, err := sql.Open(PostgreSQL, c.DbDSN)
+	require.NoError(t, err)
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	err = MigrateUp(db, SQLite)
+	require.Error(t, err)
+}
+
+func TestMigrate_WrongDriver_Sqlite(t *testing.T) {
+	var dsn = filepath.Join(t.TempDir(), testSQLiteDSN+sqliteConfig)
+
+	db, err := sql.Open(SQLite, dsn)
+	require.NoError(t, err)
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	// migrate database schema to the latest version
+	err = MigrateUp(db, PostgreSQL)
+	require.Error(t, err)
 }
